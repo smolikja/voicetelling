aa2ce96371c26ef56d8dfb0b415ada0fe9db5286
smolikja <smolikja@protonmail.com>
2022-03-04 15:19:35 +0100

    export file name change

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ def process_audio_files(grouped_files):
         for file_key in grouped_files[group_key]:
             to_be_merged += AudioSegment.from_file(grouped_files[group_key][file_key], format="mp4")
 
-        to_be_merged += AudioSegment.from_file("beep.wav", format="wav")
-        to_be_merged.export("export/{}.mp3".format(group_key.strftime("%d-%m-%Y")), format="mp3")
+        # to_be_merged += AudioSegment.from_file("beep.wav", format="wav")
+        to_be_merged.export("export/{}.mp3".format(group_key.strftime("%Y-%m-%d")), format="mp3")
     print("done")
 
 def main(args=None):
