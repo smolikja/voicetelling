b231cbfb7fb005c841c4da63d33bb38030a747da
smolikja <smolikja@protonmail.com>
2022-03-04 14:32:37 +0100

    fixed naming

--- main.py
+++ main.py
@@ -23,32 +23,32 @@ def get_voice_files():
 
     return dict_voice_files
 
-def group_files_by_date(ungrupped_files):
-    grupped_files = {}
+def group_files_by_date(ungrouped_files):
+    grouped_files = {}
 
-    for date in ungrupped_files:
+    for date in ungrouped_files:
         group_date = datetime.date(date)
         if datetime.time(date) < time(5, 0, 0):
             group_date = group_date - timedelta(days=1)
 
-        if group_date in grupped_files.keys():
-            grupped_files[group_date][date] = ungrupped_files[date]
+        if group_date in grouped_files.keys():
+            grouped_files[group_date][date] = ungrouped_files[date]
         else:
-            grupped_files[group_date] = {date : ungrupped_files[date]}
+            grouped_files[group_date] = {date : ungrouped_files[date]}
 
         # # for debug
-        # if group_date.strftime("%m/%d/%Y") in grupped_files.keys():
-        #     grupped_files[group_date.strftime("%m/%d/%Y")][date.strftime("%m/%d/%Y, %H:%M:%S")] = ungrupped_files[date]
+        # if group_date.strftime("%m/%d/%Y") in grouped_files.keys():
+        #     grouped_files[group_date.strftime("%m/%d/%Y")][date.strftime("%m/%d/%Y, %H:%M:%S")] = ungrouped_files[date]
         # else:
-        #     grupped_files[group_date.strftime("%m/%d/%Y")] = {date.strftime("%m/%d/%Y, %H:%M:%S") : ungrupped_files[date]}
+        #     grouped_files[group_date.strftime("%m/%d/%Y")] = {date.strftime("%m/%d/%Y, %H:%M:%S") : ungrouped_files[date]}
         
     # # for debug
-    # with open('grupped_files.json', 'w') as outfile:
-    #     json.dump(grupped_files, outfile)
+    # with open('grouped_files.json', 'w') as outfile:
+    #     json.dump(grouped_files, outfile)
 
-    return grupped_files
+    return grouped_files
 
-def process_audio_files(groupped_files):
+def process_audio_files(grouped_files):
     print("in progress...")
 
     try:
@@ -57,11 +57,11 @@ def process_audio_files(groupped_files):
         logging.error("Error: %s : %s" % ("export", e.strerror))
     os.mkdir("export")
 
-    for group_key in groupped_files:
+    for group_key in grouped_files:
         to_be_merged = AudioSegment.empty()
 
-        for file_key in groupped_files[group_key]:
-            to_be_merged += AudioSegment.from_file(groupped_files[group_key][file_key], format="mp4")
+        for file_key in grouped_files[group_key]:
+            to_be_merged += AudioSegment.from_file(grouped_files[group_key][file_key], format="mp4")
 
         to_be_merged += AudioSegment.from_file("beep.wav", format="wav")
         to_be_merged.export("export/{}.mp3".format(group_key.strftime("%d-%m-%Y")), format="mp3")
@@ -76,11 +76,11 @@ def main(args=None):
     # Files into dictionary {<datetime: created> : <str: file name>}
     dict_voice_files = get_voice_files()
 
-    # Files into dictionary {<datetime: grupe time> : {<datetime: created> : <str: file name>}}
-    dict_groupped_files = group_files_by_date(dict_voice_files)
+    # Files into dictionary {<datetime: group time> : {<datetime: created> : <str: file name>}}
+    dict_grouped_files = group_files_by_date(dict_voice_files)
 
     # Merge and export audio files
-    process_audio_files(dict_groupped_files)
+    process_audio_files(dict_grouped_files)
 
 if __name__ == '__main__':
     main()
