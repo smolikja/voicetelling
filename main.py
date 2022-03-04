3f3d787330300f076247c641e6bf254b741ab74c
smolikja <smolikja@protonmail.com>
2022-03-04 11:37:22 +0100

    group files by date

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import logging
 import os
-from datetime import datetime
-import logging
+from datetime import datetime, timedelta, time
+import json
 
-# Returns dictionary {date : filename} of voice files
+# Returns dictionary {<datetime: created> : <str: file name>} of voice files
 def get_voice_files():
     dict_voice_files = {}
     all_files = os.listdir()
@@ -14,12 +14,35 @@ def get_voice_files():
                 file_timestamp = file[9:19]
                 file_date = datetime.fromtimestamp(int(file_timestamp))
                 dict_voice_files[file_date] = file
-                logging.info("Detected date: {} | file name: {}".format(file_date.strftime("%d/%m/%Y, %H:%M:%S"), file))
+                logging.info("Detected file -> created: {} | file name: {}".format(file_date.strftime("%d/%m/%Y, %H:%M:%S"), file))
             except (ValueError, TypeError):
-                logging.error("Error while getting int timestamp from str | file: {}".format(file))
+                logging.error("Error while getting int timestamp from str | file name: {}".format(file))
                 break
 
     return dict_voice_files
+
+def group_files_by_date(ungrupped_files):
+    grupped_files = {}
+
+    for date in ungrupped_files:
+        group_date = datetime.date(date)
+        if datetime.time(date) < time(5, 0, 0):
+            group_date = group_date - timedelta(days=1)
+
+        if group_date in grupped_files.keys():
+            grupped_files[group_date][date] = ungrupped_files[date]
+        else:
+            grupped_files[group_date] = {date : ungrupped_files[date]}
+
+        # # for debug
+        # if group_date.strftime("%m/%d/%Y") in grupped_files.keys():
+        #     grupped_files[group_date.strftime("%m/%d/%Y")][date.strftime("%m/%d/%Y, %H:%M:%S")] = ungrupped_files[date]
+        # else:
+        #     grupped_files[group_date.strftime("%m/%d/%Y")] = {date.strftime("%m/%d/%Y, %H:%M:%S") : ungrupped_files[date]}
+        
+    # # for debug
+    # print(json.dumps(grupped_files, indent= 4))
+    return grupped_files
 
 def main(args=None):
     logging.basicConfig(filename="std.log",
@@ -27,10 +50,11 @@ def main(args=None):
                         filemode='w',
                         level= logging.DEBUG)
 
-    # TODO: nacist fily ze slozky
-    print(get_voice_files())
+    # Files into dictionary {<datetime: created> : <str: file name>}
+    dict_voice_files = get_voice_files()
 
-    # TODO: group files via date
+    # Files into dictionary {<datetime: grupe time> : {<datetime: created> : <str: file name>}}
+    dict_grupped_files = group_files_by_date(dict_voice_files)
 
     # TODO: merge skupin filu
     print("hello world")
